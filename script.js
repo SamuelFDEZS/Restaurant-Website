0f959993213635d9b45a39840c681f3441d1156d
SamuelFDEZS <thggsamuel@gmail.com>
2023-11-12 06:24:32 +0100

    Add navigation bar for menu section, and animated

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 let dropdown = document.querySelector(".header__dropdown")
-let sibling = dropdown.nextElementSibling
+let sibling = dropdown.nextElementSibling;
+let menuOptions = Array.from(document.querySelectorAll(".options__option"));
+let underline = document.querySelector(".meals__underline");
+
+menuOptions.forEach(option => {
+    option.addEventListener("click", () => {
+        let optionLeft = option.offsetLeft;
+        let optionWidth = option.offsetWidth;
+        menuOptions.forEach(option => {
+            option.classList.remove('options__option--selected')
+        })
+
+        option.classList.add('options__option--selected')
+
+        underline.style.left = `${optionLeft}px`;
+        underline.style.width = `${optionWidth}px`;
+    })
+})
+
 
 window.addEventListener("resize", () => {
     if(sibling.classList.contains("header__menu--visible")){
