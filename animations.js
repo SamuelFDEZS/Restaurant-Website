3989595b4cfc75e6fc212afe13adc511dd9cd47e
SamuelFDEZS <thggsamuel@gmail.com>
2023-11-19 00:27:56 +0100

    Fix styling issues and add menu animations

---
+++ animations.js
@@ -0,0 +1,74 @@
+let menuOptions = Array.from(document.querySelectorAll(".options__option"));
+let underline = document.querySelector(".meals__underline");
+let sliders = Array.from(document.querySelectorAll(".menu__sliders article"));
+
+
+
+const showMeals = (meal) => {
+    const selectedSlider = document.querySelector(`.sliders__${meal}`)
+
+    sliders.forEach(slider => {
+        if(slider != selectedSlider) {
+            gsap.to(slider, {
+                left: '200%',
+                duration: 0.5,
+                ease: "ease-in-out",
+            });
+        }
+    })
+    if (selectedSlider) {
+        const tl = gsap.timeline();
+        tl.to(selectedSlider, {
+            left: '50%',
+            top: '50%',
+            xPercent: -50,
+            yPercent: -50,
+            transformOrigin: "center center",
+            opacity: 1,
+            scale: 1,
+            duration: 0.5,
+            ease: "ease-in-out",
+        });
+    }
+}
+
+
+
+
+
+menuOptions.forEach(option => {
+    option.addEventListener("click", () => {
+        let optionLeft = option.offsetLeft;
+        let optionWidth = option.offsetWidth;
+        menuOptions.forEach(option => {
+            option.classList.remove('options__option--selected')
+        })
+
+        option.classList.add('options__option--selected')
+
+        underline.style.left = `${optionLeft}px`;
+        underline.style.width = `${optionWidth}px`;
+
+        const mealType = option.getAttribute("data-meal")
+        console.log(mealType)
+        showMeals(mealType)
+
+    })
+
+})
+
+
+
+//Smooth scroll
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+    console.log(e)
+})
+
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
--- script.js
+++ script.js
@@ -1,33 +1,15 @@
 let dropdown = document.querySelector(".header__dropdown")
 let sibling = dropdown.nextElementSibling;
-let menuOptions = Array.from(document.querySelectorAll(".options__option"));
-let underline = document.querySelector(".meals__underline");
-
-menuOptions.forEach(option => {
-    option.addEventListener("click", () => {
-        let optionLeft = option.offsetLeft;
-        let optionWidth = option.offsetWidth;
-        menuOptions.forEach(option => {
-            option.classList.remove('options__option--selected')
-        })
-
-        option.classList.add('options__option--selected')
-
-        underline.style.left = `${optionLeft}px`;
-        underline.style.width = `${optionWidth}px`;
-    })
-})
-
 
 window.addEventListener("resize", () => {
-    if(sibling.classList.contains("header__menu--visible")){
+    if (sibling.classList.contains("header__menu--visible")) {
         sibling.classList.remove('header__menu--visible')
     }
 })
 
 dropdown.addEventListener("click", () => {
 
-    if(sibling.classList.contains("header__menu--visible")){
+    if (sibling.classList.contains("header__menu--visible")) {
         sibling.classList.remove('header__menu--visible')
     } else {
         sibling.classList.add('header__menu--visible')
